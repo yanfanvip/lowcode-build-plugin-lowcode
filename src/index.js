f865e57c8f81881a4d887a92e84c70b8c07a9476
fritz.yan <62052598+yanfanvip@users.noreply.github.com>
2024-01-19 11:20:21 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -846,7 +846,7 @@ async function bundleEditorView(
     componentViewsImportStr = _componentViews
       .map((component) => {
         const componentNameFolder = camel2KebabComponentName(component);
-        const viewJsPath = path.resolve(rootDir, `${lowcodeDir}/${componentNameFolder}/view`).repeat(/\\/g, '\\\\');;
+        const viewJsPath = path.resolve(rootDir, `${lowcodeDir}/${componentNameFolder}/view`).replace(/\\/g, '\\\\');;
         return `import * as ${component}Data from '${viewJsPath}'`;
       })
       .join('\n');
